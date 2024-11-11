59008dcbd91026c19e5a2433bebb0da39a602f5e
edocrippaofficial <edoardocrippa96@gmail.com>
2024-11-11 12:30:47 +0100

    Added CallLogging plugin

--- src/main/kotlin/io/edocrippaofficial/Application.kt
+++ src/main/kotlin/io/edocrippaofficial/Application.kt
@@ -3,6 +3,8 @@ package io.edocrippaofficial
 import io.edocrippaofficial.configs.loadConfigMap
 import io.edocrippaofficial.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.request.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -12,6 +14,15 @@ fun main(args: Array<String>) {
 fun Application.module() {
     val configMapPath = environment.config.property("ktor.configmap.path").getString()
     val configMap = loadConfigMap(configMapPath)
+
+    install(CallLogging) {
+        format { call ->
+            val uri = call.request.uri
+            val httpMethod = call.request.httpMethod.value
+            val status = call.response.status()
+            "Response for: $httpMethod $uri: $status"
+        }
+    }
 
     configureRouting(configMap)
 }
--- src/main/kotlin/io/edocrippaofficial/controllers/HL7Controller.kt
+++ src/main/kotlin/io/edocrippaofficial/controllers/HL7Controller.kt
@@ -1,6 +1,7 @@
 package io.edocrippaofficial.controllers
 
 import io.edocrippaofficial.services.HL7Translator
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -8,7 +9,7 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class TranslateFromRequest(val message: String)
+data class TranslateFromRequest(val message: String?)
 
 fun Application.hl7Controller(
     hl7Service: HL7Translator
@@ -17,11 +18,25 @@ fun Application.hl7Controller(
         post("/translate/from") {
             val logger = call.application.environment.log
             val body = call.receive<TranslateFromRequest>()
+            val hl7message = body.message
+
+            if (hl7message.isNullOrEmpty()) {
+                logger.error("Required field message is empty")
+                call.response.status(HttpStatusCode.BadRequest)
+                call.respond(mapOf("error" to "Required field message is empty"))
+                return@post
+            }
 
             logger.trace("Body: {}", body.message)
 
-            val res = hl7Service.convertToJson(body.message)
-            call.respond(res)
+            try {
+                val res = hl7Service.convertToJson(body.message)
+                call.respond(res)
+            } catch (e: Exception) {
+                logger.error("Error during the translation", e)
+                call.response.status(HttpStatusCode.InternalServerError)
+                call.respond(mapOf("error" to e.message))
+            }
         }
     }
 }
