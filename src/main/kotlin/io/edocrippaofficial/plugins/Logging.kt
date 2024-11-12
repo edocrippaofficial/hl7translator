e7a3e0723164c47ea381ad5195eb791994d70eaf
edocrippaofficial <edoardocrippa96@gmail.com>
2024-11-12 08:32:49 +0100

    Fix logging

--- src/main/kotlin/io/edocrippaofficial/plugins/Logging.kt
+++ src/main/kotlin/io/edocrippaofficial/plugins/Logging.kt
@@ -6,8 +6,12 @@ import io.ktor.server.request.*
 import org.slf4j.MDC
 import java.util.UUID
 
+val excludedPaths = listOf("/-/healthz", "/-/ready")
+
 val Logging = createApplicationPlugin(name = "LoggingPlugin") {
     onCallReceive { call ->
+        if (excludedPaths.any { call.request.uri.startsWith(it) }) return@onCallReceive
+
         val reqId = call.request.header("X-Request-Id") ?: call.request.header("Request-Id") ?: UUID.randomUUID().toString()
         MDC.put("reqId", reqId)
 
@@ -17,6 +21,8 @@ val Logging = createApplicationPlugin(name = "LoggingPlugin") {
     }
 
     on(ResponseSent) { call ->
+        if (excludedPaths.any { call.request.uri.startsWith(it) }) return@on
+
         val uri = call.request.uri
         val httpMethod = call.request.httpMethod.value
         val status = call.response.status()
